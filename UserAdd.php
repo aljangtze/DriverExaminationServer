6fb6ef7d3327664803676d6ab4813dd08ca94994
aljangtze <aljangtze@163.com>
2020-06-04 11:58:35 +0800

    更新时区

--- UserAdd.php
+++ UserAdd.php
@@ -24,38 +24,3 @@ if ($ret == true) {
     echo json_encode($result);
 }
 return;
-/*
-if(!$session->isUserLoggedIn(true))
-{
-    echo json_encode($result);
-    return;
-}*/
-//验证token
-
-$content = file_get_contents("php://input");
-$dataInfo = json_decode($content, true);
-
-if (!$dataInfo) {
-    $result['data'] = "no data info";
-    echo json_encode($result);
-    return;
-}
-
-if (count($dataInfo) <= 0) {
-    $result['data'] = "no data info";
-    echo json_encode($result);
-    return;
-}
-
-$ret = uploadAnswerQuestions($dataInfo);
-
-if ($ret['result']) {
-    $result['result'] = 1;
-    $result['status'] = "Success";
-    $result['data'] = $ret;
-    echo json_encode($result);
-    return;
-} else {
-    $result['data'] = $ret;
-    echo json_encode($result);
-}
